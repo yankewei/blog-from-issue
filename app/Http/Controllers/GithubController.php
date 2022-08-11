9cd0be45e9be04aebcc716bdde1c5436bb995eb4
Kewei Yan <y947102435@gmail.com>
2022-08-11 14:40:08 +0800

    Update

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class GithubController extends Controller
 {
+    const GITHUB_STATE = 'github_state';
+
     public function callback(Request $request)
     {
+        if (Cache::get(self::GITHUB_STATE) !== $request->input('state')) {
+            throw new AccessDeniedHttpException('You request is timeout, please rend a request from Github.');
+        }
+
+        $response = Http::withHeaders(['Accept' => 'application/json'])->post('https://github.com/login/oauth/access_token', [
+            'client_id' => config('services.github.client_id'),
+            'client_secret' => config('services.github.client_secret'),
+            'code' => $request->input('code'),
+            'redirect_uri' => config('services.github.redirect_uri')
+        ]);
+
     }
 
     public function oauth(Request $request)
@@ -17,7 +33,9 @@ class GithubController extends Controller
             'client_id' => config('services.github.client_id'),
             'redirect_uri' => config('services.github.redirect_uri'),
             'scope' => 'read:project',
-            'state' => Str::random()
+            'state' => Cache::remember(self::GITHUB_STATE, 300, function () {
+                return Str::random();
+            }),
         ];
 
         return redirect("https://github.com/login/oauth/authorize?" . http_build_query($query));

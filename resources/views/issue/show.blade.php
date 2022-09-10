17b1e738a36b8fc6a55c56dee4228d222e62a7f1
yankewei <y947102435@gmail.com>
2022-09-10 13:09:40 +0800

    Make issue detail page better to view

--- resources/views/issue/show.blade.php
+++ resources/views/issue/show.blade.php
@@ -6,33 +6,35 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $issue->title }}</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/github.min.css') }}">
     <script src="{{ asset('js/highlight.min.js') }}"></script>
     <script src="{{ asset('js/markdown.min.js') }}"></script>
-{{--    <script>hljs.highlightAll();</script>--}}
+    <script>hljs.highlightAll();</script>
 </head>
 <body>
-<h3>{{ $issue->title }}</h3>
+    <div class="border">
+        <div class="col-6 p-2 mx-auto">
+            <p class="h3">{{ $issue->title }}</p>
+        </div>
 
-@if($issue->labels->count() !== 0)
-    <p>
-        @foreach($issue->labels as $label)
-            @if ($loop->last)
-                {{ $label->name }}
-            @else
-                {{ $label->name }}
+        @if($issue->labels->count() !== 0)
+            <div class="col-6 p-2 mx-auto">
+                @foreach($issue->labels as $label)
+                    <span class="Label Label--primary ml-1">{{ $label->name }}</span>
+                @endforeach
+                <span class="Label Label--primary ml-1">Primary</span>
+            </div>
+        @endif
+
+        <div class="col-6 p-2 mx-auto">
+            {!! parseMarkdown($issue->content) !!}
+            @if($issue->comments->count() !== 0)
+                @foreach($issue->comments as $comment)
+                    {!! parseMarkdown($comment->content) !!}
+                @endforeach
             @endif
-        @endforeach
-    </p>
-@endif
-
-<section>
-    {!! parseMarkdown($issue->content) !!}
-    @if($issue->comments->count() !== 0)
-        @foreach($issue->comments as $comment)
-            {!! parseMarkdown($comment->content) !!}
-        @endforeach
-    @endif
-</section>
+        </div>
+    </div>
 </body>
 </html>

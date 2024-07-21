c43603d692d5e033698f034e3838996df390c2c8
yankewei <y947102435@gmail.com>
2024-07-21 11:34:52 +0800

    Fix the issue open event

--- src/app/api/webhooks/github/handleIssueOpen.tsx
+++ src/app/api/webhooks/github/handleIssueOpen.tsx
@@ -1,5 +1,4 @@
 import { Issue, PrismaClient } from "@prisma/client";
-import { Tiro_Bangla } from "next/font/google";
 
 export async function handleIssueOpen(payload: object): Promise<Issue> {
   const prisma = new PrismaClient();
@@ -7,57 +6,38 @@ export async function handleIssueOpen(payload: object): Promise<Issue> {
   const repository_raw = payload["repository"];
   const issue_raw = payload["issue"];
 
-  let repository = await prisma.repository.findUnique({
-    where: {
-      full_name: repository_raw["full_name"],
-    },
-    select: {
-      id: true,
-      full_name: true,
-    },
-  });
+  const IssueOnLabel = [];
 
-  if (repository === null) {
-    repository = await prisma.repository.create({
-      data: {
-        full_name: repository_raw["full_name"],
-      },
-    });
-  }
-
-  const to_be_created_labels = [];
   if (issue_raw["labels"].length !== 0) {
     issue_raw["labels"].map(
       (label_raw: { name: String; color: String; description: String }) =>
-        to_be_created_labels.push({
-          Label: {
-            create: {
-              name: label_raw["name"],
-              description: label_raw["description"],
-              color: label_raw["color"],
-            },
+        IssueOnLabel.push({
+          where: { name: label_raw["name"] },
+          create: {
+            name: label_raw["name"],
+            description: label_raw["description"],
+            color: label_raw["color"],
           },
         }),
     );
   }
 
-  const to_be_created_issue = {
-    created_at: new Date(),
-    repositoryId: repository.id,
-    number: issue_raw["number"],
-    title: issue_raw["title"],
-    body: issue_raw["body"] ?? "",
-    IssueOnLabel: null,
-  };
-
-  if (to_be_created_labels.length !== 0) {
-    to_be_created_issue.IssueOnLabel = {
-      create: to_be_created_labels,
-    };
-  }
-
   const issue = await prisma.issue.create({
-    data: to_be_created_issue,
+    data: {
+      created_at: new Date(),
+      repository: {
+        connectOrCreate: {
+          where: { full_name: repository_raw["full_name"] },
+          create: { full_name: repository_raw["full_name"] },
+        },
+      },
+      number: issue_raw["number"],
+      title: issue_raw["title"],
+      body: issue_raw["body"] ?? "",
+      IssueOnLabel: {
+        connectOrCreate: IssueOnLabel,
+      },
+    },
   });
 
   return issue;

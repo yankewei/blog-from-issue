bce45a3381ed585a58abf56458b4e227d1e924a2
yankewei <y947102435@gmail.com>
2024-08-14 13:05:54 +0800

    Fix the issue that `useState` cannot inside the callback

--- src/ui/home/List.tsx
+++ src/ui/home/List.tsx
@@ -6,16 +6,23 @@ import { Issue } from "@prisma/client";
 import { useState } from "react";
 
 export default function List({ issues }: { issues: Array<Issue> }) {
+  const [issue_id, setIssueId] = useState(0);
 
   const items = issues.map(function (issue): React.ReactElement {
-  const [hover, setHover] = useState(false);
-
     const href = `issues/${issue.id}`;
     return (
-      <ListboxItem key={issue.id} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+      <ListboxItem
+        key={issue.id}
+        onMouseEnter={() => setIssueId(issue.id)}
+        onMouseLeave={() => setIssueId(0)}
+      >
         <Link href={href} className="flex flex-row items-center justify-start">
           <p className="flex flex-row gap-2 items-center">
-            <span className={hover ? "text-black" : "text-gray-500"}>{issue.title} </span>
+            <span
+              className={issue_id === issue.id ? "text-black" : "text-gray-500"}
+            >
+              {issue.title}{" "}
+            </span>
             <span className="text-gray-400 text-sm">
               {issue.created_at.toLocaleDateString("en-US", {
                 year: "numeric",
@@ -32,11 +39,7 @@ export default function List({ issues }: { issues: Array<Issue> }) {
   return (
     <div className="flex flex-col items-center">
       <ListboxWrapper>
-        <Listbox
-          aria-label="Listbox Variants"
-          color="primary"
-          variant="light"
-        >
+        <Listbox aria-label="Listbox Variants" color="primary" variant="light">
           {items}
         </Listbox>
       </ListboxWrapper>
